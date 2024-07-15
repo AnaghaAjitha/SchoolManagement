1d14b7e7025fa50ba58e8049eca51f0ad8c5b187
AnaghaAjitha <168955434+AnaghaAjitha@users.noreply.github.com>
2024-07-15 21:17:21 +0530

    Update urls.py

--- school/urls.py
+++ school/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django admin URLs
-    path('api/', include('schoolSystem.urls')),  # Include URLs from the schoolSystem app
+    path('api/', include('schoolSystem.urls')), 
 ]
