70511fcb8b4d0b339af25fd52aa684cdf1310925
AnaghaAjitha <168955434+AnaghaAjitha@users.noreply.github.com>
2024-07-27 23:57:54 +0530

    Update views.py

--- schoolSystem/views.py
+++ schoolSystem/views.py
@@ -2,8 +2,8 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
-from .models import User,Subject,Mark
-from .serializers import UserSerializer,SubjectSerializer,MarkSerializer
+from .models import *
+from .serializers import *
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,4 +18,46 @@ class MarkViewSet(viewsets.ModelViewSet):
     queryset=Mark.objects.all()
     serializer_class=MarkSerializer
     
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
     
+class StaffViewSet(viewsets.ModelViewSet):
+    queryset=Staff.objects.all()
+    serializer_class=StaffSerializer
+    
+class ClassViewSet(viewsets.ModelViewSet):
+    queryset=Class.objects.all()
+    serializer_class=ClassSerializer
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset=Course.objects.all()
+    serializer_class=CourseSerializer
+    
+class ExamViewSet(viewsets.ModelViewSet):
+    queryset=Exam.objects.all()
+    serializer_class=ExamSerializer
+    
+class StudentAttendenceViewSet(viewsets.ModelViewSet):
+    queryset=StudentAttendence.objects.all()
+    serializer_class=StudentAttendanceSerializer
+    
+class StaffAttendenceViewSet(viewsets.ModelViewSet):
+    queryset=StaffAttendence.objects.all()
+    serializer_class=StaffAttendanceSerializer
+    
+class EventViewSet(viewsets.ModelViewSet):
+    queryset=Event.objects.all()
+    serializer_class=EventSerializer
+    
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset=Room.objects.all()
+    serializer_class=RoomSerializer
+    
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset=Enrollment.objects.all()
+    serializer_class=EnrollmentSerializer
+
+class ScheduleViewSet(viewsets.ModelViewSet):
+    queryset=Schedule.objects.all()
+    serializer_class=ScheduleSerializer
